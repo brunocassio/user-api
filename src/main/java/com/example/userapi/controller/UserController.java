cc7d68640fa98389a2d725472cb7ba7e3fa6ee0b
Bruno Cássio Ferreira de Freitas <bruno@MacBook-Pro.local>
2023-12-20 10:23:35 -0300

    Initial commit

---
+++ src/main/java/com/example/userapi/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.example.userapi.controller;
+
+import com.example.userapi.dto.UserDTO;
+import com.example.userapi.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    public static List<UserDTO> usuarios = new ArrayList<>();
+
+    @GetMapping
+    public List<UserDTO> getUsers() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO findById(@PathVariable Long id) {
+        return userService.findById(id); }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO newUser(@RequestBody @Valid UserDTO userDTO) {
+        return userService.save(userDTO); }
+
+    @GetMapping("/{cpf}/cpf")
+    public UserDTO findByCpf(@PathVariable String cpf) {
+        return userService.findByCpf(cpf); }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+
+    @GetMapping("/search")
+    public List<UserDTO> queryByName(@RequestParam(name="nome", required = true) String nome) {
+        return userService.queryByName(nome);
+    }
+
+    @PatchMapping("/{id}")
+    public UserDTO editUser(@PathVariable Long id,
+                            @RequestBody UserDTO userDTO) {
+        return userService.editUser(id, userDTO);
+    }
+
+    @GetMapping("/pageable")
+    public Page<UserDTO> getUsersPage(Pageable pageable) {
+        return userService.getAllPage(pageable); }
+
+}
---
+++ src/main/java/com/example/userapi/model/User.java
@@ -0,0 +1,37 @@
+package com.example.userapi.model;
+
+import com.example.userapi.dto.UserDTO;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    private long id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+
+    public static User convert(UserDTO userDTO) {
+        User user = new User();
+        user.setNome(userDTO.getNome());
+        user.setEndereco(userDTO.getEndereco());
+        user.setCpf(userDTO.getCpf());
+        user.setEmail(userDTO.getEmail());
+        user.setTelefone(userDTO.getTelefone());
+        user.setDataCadastro(userDTO.getDataCadastro());
+        return user;
+    }
+}
